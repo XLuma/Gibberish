2eba2a36156ba198c59ec805c9a86c8846b47803
XLuma <39510265+XLuma@users.noreply.github.com>
2022-07-10 22:29:17 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Gibberish
+Gibberish is a simple program to "poorly translate" text from msbt files. It achieves this by using the Deepl api to translate text to foreign languages back to english.
+
+# Dependencies
+- to use this, you will need python, deepl, pymsyt.
+
+# Usage
+
+- Put the necessary files in the "translate" folder. then, open the program and change the source language as well as how many times it should be translated. (The lower the value, the less weird it is likely to be)
+- You will also need a Deepl api key. You can signup for their free plan which allows 500k characters per month, which should be plenty. Put it in the required field in the program.
+- Then simply launch the program, and wait. An "output" folder will appear with new files, having translated text. 
+
---
+++ gibberish.py
@@ -0,0 +1,58 @@
+from ctypes import sizeof
+from logging import root
+from random import randrange
+import googletrans
+from pymsyt import Msbt
+import deepl
+import os
+LANGUAGE = "en" #change this to your source language
+API_KEY = "7433bcab-0b64-3696-d650-e5ae709fa9b3" #add you Deepl api key here
+CYCLES = 6 #how many times text should be translated before going back to english. adjust this to taste
+languages = ["BG", "CS", "DA", "DE", "ES", "ET", "FI", "FR", "HU", "ID", "IT", "JA", "LT", "LV", "NL", "PL", "PT-PT","PT-BR", "RO", "RU", "SK", "SL", "SV", "TR", "ZH"]
+current_language = ""
+curent_lang_index = 0
+rootdir = os.getcwd()
+rootdir = rootdir + "\\translate"
+outdir = os.getcwd() + "\\output"
+
+def pick_language():
+    i = randrange(0, len(googletrans.LANGCODES))
+    return i, googletrans.LANGCODES[i]
+
+
+def main():
+    translator = deepl.Translator(API_KEY)
+    google_trans = googletrans.Translator()
+    for subdir, dirs, files in os.walk(rootdir):
+        for file in files:
+            outfile = outdir  + str.split(subdir, "\\translate")[1] + "\\" + file
+            data = open(os.path.join(subdir, file), "rb").read()
+            msbt = Msbt.from_binary(data)
+            msbt_dict = msbt.to_dict()
+            for entry, contents in msbt_dict["entries"].items(): # Iterate MSBT text entries
+                print(entry)
+                for texts in contents.values():
+                    for stuff in texts:
+                        if 'text' in stuff: #lmao finally got them strings
+                            currentString = stuff['text']
+                            #result = translator.translate_text(currentString, target_lang=LANGUAGE)
+                            result = google_trans.translate(currentString, dest='en')
+                            for i in range(CYCLES):
+                                curent_lang_index, current_language = pick_language()
+                                #result = translator.translate_text(result.text, target_lang=current_language) #have a random function to pick a language
+                                result = google_trans.translate(result.text, dest=current_language)
+                            #outstr = translator.translate_text(result.text, target_lang=LANGUAGE)
+                            outstr = google_trans.translate(result.text, dest=LANGUAGE)
+                            #print(outstr.text)
+                            msbt_dict["entries"][entry] = { # Adding a new text entry
+                                "contents": [{"text":outstr.text}]
+                            }
+            os.makedirs(outdir + str.split(subdir, "\\translate")[1], exist_ok=True)
+            open(outfile, 'wb').write( Msbt.from_dict(msbt_dict).to_binary(big_endian=True))
+                                
+
+    
+
+
+if __name__ == "__main__":
+    main()
